40ffb98063a1041f1a18fe72fdb924300806b866
hupe1980 <frankhuebner1980@gmail.com>
2024-06-24 00:23:23 +0200

    Initial commit

--- torchadv/attacks/__init__.py
+++ torchadv/attacks/__init__.py
@@ -1,7 +1,9 @@
+from .ffgsm import FFGSM
 from .fgsm import FGSM
 from .pgd import PGD
 
 __all__ = [
+    "FFGSM",
     "FGSM",
     "PGD",
 ]
--- torchadv/attacks/ffgsm.py
+++ torchadv/attacks/ffgsm.py
@@ -9,18 +9,19 @@ from ..utils import clip_tensor, normalize_gradients
 
 
 @dataclass(kw_only=True)
-class FGSM(Attack):
+class FFGSM(Attack):
     """
-    Fast Gradient Sign Method (FGSM) attack class.
+    Fast Gradient Sign Method (FGSM) combined with random initialization attack class.
     Inherits from Attack class.
 
     Papers:
-        - https://arxiv.org/abs/1412.6572
+        - https://arxiv.org/abs/2001.03994
     """
 
     eps: float = 8/255  # Maximum perturbation magnitude
+    alpha: float = 10/255 # Step size for gradient update
     norm: float | int = np.inf  # Norm to use for perturbation calculation
-    clip_min: float | None = None # Minimum value for clipping adversarial examples
+    clip_min: float | None = None  # Minimum value for clipping adversarial examples
     clip_max: float | None = None  # Maximum value for clipping adversarial examples
     criterion: Callable = field(
         default_factory=lambda: torch.nn.CrossEntropyLoss()
@@ -28,7 +29,7 @@ class FGSM(Attack):
 
     def _call_impl(self, inputs: torch.Tensor, labels: torch.Tensor | None = None, **kwargs):
         """
-        Implementation of the FGSM attack.
+        Implementation of the FGSM combined with random initialization attack.
 
         Args:
             inputs (torch.Tensor): The input tensor (original example).
@@ -38,23 +39,33 @@ class FGSM(Attack):
         Returns:
             torch.Tensor: The adversarially perturbed input tensor (adversarial example).
         """
-        inputs = inputs.clone().detach().requires_grad_(True)
+        inputs = inputs.clone().detach()
 
         is_targeted = labels is not None
         if not is_targeted:
             _, labels = torch.max(self.get_logits(inputs), 1)
 
-        loss = self.criterion(self.get_logits(inputs), labels)
+        # Add random noise for initialization
+        adv = inputs + torch.randn_like(inputs).uniform_(-self.eps, self.eps)
+        
+        # Clip adversarial example to ensure it stays within specified bounds
+        adv = clip_tensor(adv, min_val=self.clip_min, max_val=self.clip_max)
+        
+        # Enable gradient computation on adversarial example
+        adv.requires_grad = True
+
+        # Compute loss using criterion
+        loss = self.criterion(self.get_logits(adv), labels)
 
         if is_targeted:
             # Invert loss for targeted attack
             loss = -loss
 
-        # Compute gradient of loss w.r.t. inputs tensor
-        grad = torch.autograd.grad(loss, inputs, retain_graph=False, create_graph=False)[0]
+        # Compute gradient of loss w.r.t. adversarial example
+        grad = torch.autograd.grad(loss, adv, retain_graph=False, create_graph=False)[0]
 
-        # Normalize gradient and scale by epsilon to get perturbation
-        optimal_perturbation = normalize_gradients(grad, self.norm) * self.eps
+        # Normalize gradient and scale by alpha to get perturbation
+        optimal_perturbation = normalize_gradients(grad, self.norm) * self.alpha
 
         # Add perturbation to original example to obtain adversarial example
         adv = inputs + optimal_perturbation
